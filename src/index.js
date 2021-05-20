8ab2a233d13b26da9a774a111375804bc61e59b2
Iryna Konoplova <a.irina773@gmail.com>
2021-05-21 01:37:59 +0300

    Добавляет хендлбар

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+
+
+
+fetch('https://restcountries.eu/rest/v2/name/Colombia')
+    .then(response => {
+    return response.json();
+    })
+    .then(countri => {
+        console.log(countri);
+    })
+    .catch(error => {
+        console.log(error);
+    });
