e4ca85487f2ac4a9313d6b282a19ee8f8f099208
Iryna Konoplova <a.irina773@gmail.com>
2021-05-22 02:09:25 +0300

    Рендерит разметку

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import './sass/main.scss';
+import countriCardTpl from './templates/countri-card.hbs';
+
+const refs = {
+    cardContainer: document.querySelector('.js-card-container'),
+    searchForm: document.querySelector('.js-search-form')
+}
+
+refs.searchForm.addEventListener('input', onSearch)
+
+function onSearch(e) {
+    e.preventDefault();
+
+    // const form = e.currentTarget;
+    // const searchQuery = form.elements.query.value
+
+    fetchCountri('Col')
+    .then(renderCountriCard)
+    .catch(error => { console.log(error)});
+}
 
 
 
-fetch('https://restcountries.eu/rest/v2/name/Colombia')
-    .then(response => {
-    return response.json();
-    })
-    .then(countri => {
-        console.log(countri);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+
+function fetchCountri(countryAlpha) {
+    return fetch(`https://restcountries.eu/rest/v2/alpha/${countryAlpha}`)
+        .then(response => {
+            return response.json();
+        }); 
+    }
+
+function renderCountriCard(countri) {
+        const markup = countriCardTpl(countri);
+        refs.cardContainer.innerHTML = markup;
+    }
